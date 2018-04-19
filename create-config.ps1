1e1ac69e20abf5d1e681373d674514f2265cdfaa
Tom Lijnse <tom.lijnse@layerzero.nl>
2018-04-19 13:48:43 +0200

    First commit

---
+++ create-config.ps1
@@ -0,0 +1,22 @@
+﻿# Create a full sample UCS configuration from scratch using PowerShell
+
+# Import UCS Powertool Module
+Import-Module Cisco.UCSManager
+
+# Declare global variables
+$site_id = 1
+$pod_id = 1
+
+$hostname_prefix = "ucspe-"
+
+# Connect to UCS Manager using a session xml file and a secure key defined in a key file.
+$key = ConvertTo-SecureString (Get-Content .\ucspe.key)
+$handle = Connect-Ucs -Key $key -LiteralPath .\ucspe.xml
+
+# Set UCS system name
+# This uses the generic Set-UcsManagedObject method, because no specific cmdlet exists
+$hostname = $hostname_prefix + $site_id + $pod_id
+Get-UcsManagedObject -Dn sys | Set-UcsManagedObject -PropertyMap @{name = $hostname} -Force
+
+# Disconnect from UCS Manager
+Disconnect-Ucs -Ucs $handle
