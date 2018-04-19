333157ce2592fc886b9835644748d1189c23edfe
Tom Lijnse <tom.lijnse@layerzero.nl>
2018-04-19 14:20:48 +0200

    Added suborganizations

--- create-config.ps1
+++ create-config.ps1
@@ -7,9 +7,11 @@ Import-Module Cisco.UCSManager
 $site_id = 1
 $pod_id = 1
 
+$environments = @("dev", "tst", "acc", "prd")
+
 $hostname_prefix = "ucspe-"
 
-# Connect to UCS Manager using a session xml file and a secure key defined in a key file.
+# Connect to UCS Manager using a session xml file and a secure key defined in a key file
 $key = ConvertTo-SecureString (Get-Content .\ucspe.key)
 $handle = Connect-Ucs -Key $key -LiteralPath .\ucspe.xml
 
@@ -17,6 +19,14 @@ $handle = Connect-Ucs -Key $key -LiteralPath .\ucspe.xml
 # This uses the generic Set-UcsManagedObject method, because no specific cmdlet exists
 $hostname = $hostname_prefix + $site_id + $pod_id
 Get-UcsManagedObject -Dn sys | Set-UcsManagedObject -PropertyMap @{name = $hostname} -Force
+
+# Create suborganizations
+foreach ($env in $environments) {
+    $org = Get-UcsOrg -Name $env
+    if (-Not $org){
+        Get-UcsOrg -Level root | Add-UcsOrg $env
+    }
+}
 
 # Disconnect from UCS Manager
 Disconnect-Ucs -Ucs $handle
